4076d19c4267d08b8d82e98bdfb63ca4af7358dd
Ainazik <98087256+AinazikS@users.noreply.github.com>
2022-03-07 15:54:45 +0600

    Update index.js

--- 10hours/index.js
+++ 10hours/index.js
@@ -1,55 +1,81 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react'
+import ReactDOM from 'react-dom'
 //CSS
-import "./index.css";
+import './index.css'
 //Objects
-const firstBook = {
-  title:"History of past , present and future",
-  date: "20.07.2022",
-  author: " Michael Jakson ",
-  img: "https://upload.wikimedia.org/wikipedia/ru/thumb/d/d5/Michael_Jackson_HIStory_Book_I.jpeg/274px-Michael_Jackson_HIStory_Book_I.jpeg",
-};
-const secondBook = {
-  title:'Winner of the Pulizer Prize',
-  date: "25.12.2022",
-  author: "Harrer Lee",
-  img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZfbqwkxpwDMigyoxsmEAErhWqgeFB-NOgTQ&usqp=CAU",
-};
-// let date = "20.07.2022";
-// const title = " Michael Jakson ";
-// const img = "img link here"
+const books = [
+  {
+    id: 1,
+    title: 'History of past , present and future',
+    date: '20.07.2022',
+    author: ' Michael Jakson ',
+    img: 'https://upload.wikimedia.org/wikipedia/ru/thumb/d/d5/Michael_Jackson_HIStory_Book_I.jpeg/274px-Michael_Jackson_HIStory_Book_I.jpeg',
+  },
+  {
+    id: 2,
+    title: 'Winner of the Pulizer Prize',
+    date: '25.12.2022',
+    author: 'Harrer Lee',
+    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZfbqwkxpwDMigyoxsmEAErhWqgeFB-NOgTQ&usqp=CAU',
+  },
+  {
+    id: 3,
+    title: 'Bless the Daughter Raised by a Voice in Her Head',
+    date: '30.10.2022',
+    author: 'Warsan Shire',
+    img: 'https://covers.powells.com/9780593134351.jpg',
+  },
+  {
+    id: 4,
+    title: 'Rise',
+    date: '05.09.2022',
+    author: 'Jeff Yang, Phil Yu, and Philip Wang',
+    img: 'https://covers.powells.com/9780358508090.jpg',
+  },
+  {
+    id: 5,
+    title: 'Abolition for the People',
+    date: '16.01.2022',
+    author: 'Colin Kaepernick',
+    img: 'https://covers.powells.com/9781595911162.jpg',
+  },
+  {
+    id: 6,
+    title: 'Was It Worth It?',
+    date: '19.04.2022',
+    author: ' Doug Peacock',
+    img: 'https://covers.powells.com/9781952338045.jpg',
+  },
+]
 
 function Booklist() {
   return (
-    <section className="booklist">
-      <Book
-        title={firstBook.title}
-        img={firstBook.img}
-        author={firstBook.author}
-        date={firstBook.date}
-      />
-      <Book
-        title={secondBook.title}
-        img={secondBook.img}
-        author={secondBook.author}
-        date={secondBook.date}
-      />
+    <section className='booklist'>
+      {books.map((book) => {
+        const { img, title, date, author } = book
+        return (
+          <div>
+            <Book book={book}></Book>
+          </div>
+        )
+      })}
     </section>
-  );
+  )
 }
 
-const Book = (properties) => {
-  console.log(properties);
+const Book = (props) => {
+  const { title, author, date, img } = props.book
   return (
-    <article className="book">
-      <img src={properties.img} alt="" />
-      <h1>{properties.title}</h1>
-      <h4>{properties.author.toLocaleUpperCase()}</h4>
-      <h6>{properties.date}</h6>
-      <p>{properties.job}</p>
+    <article className='book'>
+      <img src={img} alt='' />
+      <h1>{title}</h1>
+      <h4>{author}</h4>
+      <h6>{date}</h6>
     </article>
-  );
-};
+  )
+}
+
+ReactDOM.render(<Booklist />, document.getElementById('root'))
 
 
-ReactDOM.render(<Booklist />, document.getElementById("root"));
+
